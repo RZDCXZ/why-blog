e9c1ec6c56b4b030f25a8c9b8ab609d07a52f73c
rzdcxz <1790279527@qq.com>
2024-05-23 15:54:44 +0800

    更改astro配置文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://why-myblog.vercel.app/",
   base: "/",
   trailingSlash: "always",
   integrations: [
