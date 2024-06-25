ebeeb2f0bc5aac98d19e2cba0d253ba88fb0fed8
rzdcxz <1790279527@qq.com>
2024-06-25 15:42:16 +0800

    https测试

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo docker pull rzdcxz/why-blog
 sudo docker stop why-blog-container
 sudo docker rm why-blog-container
-sudo docker run --name why-blog-container -d -p 80:80 -v /home/ubuntu/nginx/conf.d:/etc/nginx/conf.d rzdcxz/why-blog
+sudo docker run --name why-blog-container -d -p 80:80 -v /home/ubuntu/nginx:/etc/nginx rzdcxz/why-blog
 sudo docker image prune -f
 sudo docker container prune -f
